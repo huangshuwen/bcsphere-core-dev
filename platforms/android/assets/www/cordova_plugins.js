7b3ca4c838efa161b7d91ce59a72d5df6723586b
lizhi <zhi.li@jumacc.com>
2014-03-27 16:31:47 +0800

    fix the startScan bug on android 4.3 platform

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,25 +1,25 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/org.bcsphere.bluetooth/www/bluetooth.js",
-        "id": "org.bcsphere.bluetooth.bluetooth",
-        "merges": [
-            "navigator.bluetooth"
-        ]
-    },
-    {
         "file": "plugins/org.bcsphere.wifi/www/wifi.js",
         "id": "org.bcsphere.wifi.wifi",
         "merges": [
             "navigator.wifi"
+        ]
+    },
+    {
+        "file": "plugins/org.bcsphere.bluetooth/www/bluetooth.js",
+        "id": "org.bcsphere.bluetooth.bluetooth",
+        "merges": [
+            "navigator.bluetooth"
         ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "org.bcsphere.bluetooth": "0.1.0",
-    "org.bcsphere.wifi": "0.0.1"
+    "org.bcsphere.wifi": "0.0.1",
+    "org.bcsphere.bluetooth": "0.1.0"
 }
 // BOTTOM OF METADATA
 });
